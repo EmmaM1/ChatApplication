081c2b5fa7f723617db0dfc858e6a57db886dc5c
EmmaM1 <92796525+EmmaM1@users.noreply.github.com>
2021-11-08 22:32:09 +0000

    Reformatting

--- Server.java
+++ Server.java
@@ -9,26 +9,26 @@ public class Server {
   public static void main(String[] args) {
     try {
       ServerSocket serverSocket = new ServerSocket(1400);
-        System.out.println("Waiting for client...");
-        Socket server = serverSocket.accept();
-        System.out.println("Connected to " + server.getRemoteSocketAddress());
-        DataInputStream input = new DataInputStream(server.getInputStream());
-        String user = input.readUTF().split(" ")[0];
-        System.out.println(user + " has joined");
-        DataOutputStream output = new DataOutputStream(server.getOutputStream());
-        output.writeUTF("Connected to " + server.getLocalSocketAddress());
-        while (true) {
-          try {
-            System.out.println(input.readUTF());
-          } catch (IOException e) {
-            System.out.println(user + " has disconnected");
-            serverSocket.close();
-            break;
-          }
+      System.out.println("Waiting for client...");
+      Socket server = serverSocket.accept();
+      System.out.println("Connected to " + server.getRemoteSocketAddress());
+      DataInputStream input = new DataInputStream(server.getInputStream());
+      String user = input.readUTF();
+      System.out.println(user + " has joined");
+      DataOutputStream output = new DataOutputStream(server.getOutputStream());
+      output.writeUTF("Connected to " + server.getLocalSocketAddress());
+      while (true) {
+        try {
+          System.out.println(input.readUTF());
+        } catch (IOException e) {
+          System.out.println(user + " has disconnected");
+          serverSocket.close();
+          break;
         }
-      } catch (IOException e) {
-        e.printStackTrace();
       }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
 }
